b97c83a7748da94ab66f21f877ef42737fb02a2f
Matheus Avelino <matheusavelino10@edu.unifor.br>
2022-10-26 21:43:33 +0000

    1.2

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
+
+
 app.get('/', (req, res) => {
-  res.send('Hello Express app!')
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
+
+
 
 app.listen(3000, () => {
   console.log('server started');
